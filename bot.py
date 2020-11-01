bf918731540117c0de7ffe7b459c2af28b2e8331
Dali <dali.gaoudi@gmail.com>
2020-11-02 00:44:55 +0100

    Deli

--- bot.py
+++ bot.py
@@ -5,36 +5,50 @@ import asyncio
 
 client = commands.Bot(command_prefix='>')
 
+urls = {
+
+}
+
+
 @client.event
 async def on_ready():
     print('bot is ready')
-    
-    
+
+
 @client.event
 async def on_member_join(member):
     print(f'(member) joina w walla kahboun')
-    
-    
+
+
 @client.event
 async def on_member_remove(member):
-        print(f'(member)khraj yaatih aasba')
-        
+    print(f'(member)khraj yaatih aasba')
+
 
 @client.command()
 async def aasba(ctx):
     await ctx.send('yaatek aasba!')
-    
-    
+
 
 @client.command()
-async def aasbaenh(ctx,target:discord.Member):
-    await ctx.send('yaatek aasba ala sormek ya!' + target.mention)    
-    
-  
-                
+async def aasbaenh(ctx, target: discord.Member):
+    await ctx.send('yaatek aasba ala sormek ya!' + target.mention)
+
+
 @client.command()
 async def nhebnalaab(ctx):
     await ctx.send('@everyone ya malhet chkn yalaab')
-                
 
-client.run('NzY5MzAyOTY3ODAzMTgzMTI0.X5NDOA.BlB4R8IEEmCgQNOWcy9ml1EN5Dk')   
+
+@client.command()
+async def join(ctx):
+    channel = ctx.author.voice.channel
+    await channel.connect()
+
+
+@client.command()
+async def leave(ctx):
+    await ctx.voice_client.disconnect()
+
+
+client.run('NzY5MzAyOTY3ODAzMTgzMTI0.X5NDOA.BlB4R8IEEmCgQNOWcy9ml1EN5Dk')
