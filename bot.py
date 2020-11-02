c09d819cc318c5a939f15c223b797dcf3aac4a83
Dali <dali.gaoudi@gmail.com>
2020-11-02 02:24:40 +0100

    D

--- bot.py
+++ bot.py
@@ -4,6 +4,8 @@ from discord.utils import get
 import asyncio
 import os
 import youtube_dl
+
+
 client = commands.Bot(command_prefix='>')
 
 urls = {
@@ -12,6 +14,24 @@ urls = {
     "ebeedzebi": "https://youtu.be/LuVb5mFL-3s",
     "tetmaneyk": "https://youtu.be/2TpSjkgfoJI"
 }
+
+OPUS_LIBS = ['libopus-0.x86.dll', 'libopus-0.x64.dll',
+             'libopus-0.dll', 'libopus.so.0', 'libopus.0.dylib']
+
+
+def load_opus_lib(opus_libs=OPUS_LIBS):
+    if opus.is_loaded():
+        return True
+
+    for opus_lib in opus_libs:
+        try:
+            opus.load_opus(opus_lib)
+            return
+        except OSError:
+            pass
+
+        raise RuntimeError('Could not load an opus lib. Tried %s' %
+                           (', '.join(opus_libs)))
 
 
 @client.event
