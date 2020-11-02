409fd27d67d627364e9daf9439c25a8ea9ca9b6e
Dali <dali.gaoudi@gmail.com>
2020-11-02 01:01:55 +0100

    Deli

--- bot.py
+++ bot.py
@@ -2,11 +2,15 @@ import discord
 from discord.ext import commands
 from discord.utils import get
 import asyncio
-
+import os
+import youtube_dl
 client = commands.Bot(command_prefix='>')
 
 urls = {
-
+    "rwayak": "https://youtu.be/3nPrRUV7L4o",
+    "tnayekwahdek": "https://youtu.be/vUWn2Dh3o6c",
+    "ebeedzebi": "https://youtu.be/LuVb5mFL-3s",
+    "tetmaneyk": "https://youtu.be/2TpSjkgfoJI"
 }
 
 
@@ -44,11 +48,107 @@ async def nhebnalaab(ctx):
 async def join(ctx):
     channel = ctx.author.voice.channel
     await channel.connect()
+    await ctx.send("haw jit!")
 
 
 @client.command()
 async def leave(ctx):
     await ctx.voice_client.disconnect()
+    await ctx.send("maadch kdar f hel bled")
 
+
+@client.command()
+async def jomlaop(ctx, place):
+    await ctx.send("O wras rabek manji " + place+" o kadrek namsah bih sorm omek li tneket meno mn shtar rjel touns")
+
+
+@client.command(pass_context=True, aliases=['p', 'pla'])
+async def play(ctx, url: str):
+
+    song_there = os.path.isfile("song.mp3")
+    try:
+        if song_there:
+            os.remove("song.mp3")
+            print("Removed old song file")
+    except PermissionError:
+        print("Trying to delete song file, but it's being played")
+        await ctx.send("ERROR: Music playing")
+        return
+
+    await ctx.send("Getting everything ready now")
+
+    voice = get(client.voice_clients, guild=ctx.guild)
+
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+    }
+
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        print("Downloading audio now\n")
+        ydl.download([url])
+
+    for file in os.listdir("./"):
+        if file.endswith(".mp3"):
+            name = file
+            print(f"Renamed File: {file}\n")
+            os.rename(file, "song.mp3")
+
+    voice.play(discord.FFmpegPCMAudio("song.mp3"),
+               after=lambda e: print("Song done!"))
+    voice.source = discord.PCMVolumeTransformer(voice.source)
+    voice.source.volume = 0.07
+
+    nname = name.rsplit("-", 2)
+    print("playing\n")
+
+
+@client.command(pass_context=True)
+async def bsout(ctx, vid):
+    url = urls[vid]
+    song_there = os.path.isfile("song.mp3")
+    try:
+        if song_there:
+            os.remove("song.mp3")
+            print("Removed old song file")
+    except PermissionError:
+        print("Trying to delete song file, but it's being played")
+        await ctx.send("ERROR: Music playing")
+        return
+
+    await ctx.send("Getting everything ready now")
+
+    voice = get(client.voice_clients, guild=ctx.guild)
+
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+    }
+
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        print("Downloading audio now\n")
+        ydl.download([url])
+
+    for file in os.listdir("./"):
+        if file.endswith(".mp3"):
+            name = file
+            print(f"Renamed File: {file}\n")
+            os.rename(file, "song.mp3")
+
+    voice.play(discord.FFmpegPCMAudio("song.mp3"),
+               after=lambda e: print("Song done!"))
+    voice.source = discord.PCMVolumeTransformer(voice.source)
+    voice.source.volume = 0.07
+
+    nname = name.rsplit("-", 2)
+    print("playing\n")
 
 client.run('NzY5MzAyOTY3ODAzMTgzMTI0.X5NDOA.BlB4R8IEEmCgQNOWcy9ml1EN5Dk')
